3ce4805b357fe64ae0ac1451cf661f11a7db044a
neilaquilina <6985772+neilaquilina@users.noreply.github.com>
2020-10-28 14:30:03 +0100

    Created Move() Script
    Created Move() Script to move the Player around on x and y axis

---
+++ LaserDefenderSWD42B/Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+using UnityEngine.XR.WSA.Input;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    //moves the Player ship
+    private void Move()
+    {
+        //var changes its variable type
+        //depending on what I save in it
+        //deltaX will have the difference in the x-axis which the Player moves
+        var deltaX = Input.GetAxis("Horizontal");
+
+        //newXPos   = current x-position   + difference in x
+        var newXPos = transform.position.x + deltaX;
+
+        //the above in y axis:
+        var deltaY = Input.GetAxis("Vertical");
+        var newYPos = transform.position.y + deltaY;
+
+        //move the Player ship to the newXPos
+        this.transform.position = new Vector2(newXPos, newYPos);
+
+          
+
+
+
+    }
+}
