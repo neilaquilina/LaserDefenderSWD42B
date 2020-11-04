9af045ecb63090ec9c95cbab9be1d9f8cb4d66fe
neilaquilina <6985772+neilaquilina@users.noreply.github.com>
2020-11-04 12:35:43 +0100

    ViewPorttoWorldPoint()

--- LaserDefenderSWD42B/Assets/Scripts/Player.cs
+++ LaserDefenderSWD42B/Assets/Scripts/Player.cs
@@ -8,10 +8,17 @@ using UnityEngine.XR.WSA.Input;
 
 public class Player : MonoBehaviour
 {
+    
+    //makes the variable editable from Unity Editor
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 0.7f;
+
+    float xMin, xMax, yMin, yMax;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpMoveBoundaries();
     }
 
     // Update is called once per frame
@@ -20,27 +27,42 @@ public class Player : MonoBehaviour
         Move();
     }
 
+
+    private void SetUpMoveBoundaries()
+    {
+        //get the Main Camera from Unity
+        Camera gameCamera = Camera.main;
+
+        //set the boundaries on the x-axis
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        //set the boundaries on the y-axis
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
     //moves the Player ship
     private void Move()
     {
         //var changes its variable type
         //depending on what I save in it
         //deltaX will have the difference in the x-axis which the Player moves
-        var deltaX = Input.GetAxis("Horizontal");
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
 
         //newXPos   = current x-position   + difference in x
         var newXPos = transform.position.x + deltaX;
+        //apply boundaries on x-axis
+        newXPos = Mathf.Clamp(newXPos, xMin, xMax);
 
         //the above in y axis:
-        var deltaY = Input.GetAxis("Vertical");
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         var newYPos = transform.position.y + deltaY;
+        newYPos = Mathf.Clamp(newYPos, yMin, yMax);
 
         //move the Player ship to the newXPos
         this.transform.position = new Vector2(newXPos, newYPos);
 
-          
-
-
-
+        
     }
 }
