b3a3ee38307831861fd502b481f23de0d94af8b2
neilaquilina <6985772+neilaquilina@users.noreply.github.com>
2020-11-11 14:17:16 +0100

    Move Enemy on Path
    1. Lists
    2. Waypoints
    3. Vector2.MoveTowards()

--- LaserDefenderSWD42B/Assets/Scripts/EnemyPathing.cs
+++ LaserDefenderSWD42B/Assets/Scripts/EnemyPathing.cs
@@ -7,15 +7,53 @@ public class EnemyPathing : MonoBehaviour
     //a list of points of type Transform
     [SerializeField] List<Transform> waypoints;
 
+    [SerializeField] float enemyMoveSpeed = 2f;
+
+    //shows the next waypoint
+    int waypointIndex = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //set the starting position of the Enemy ship to the position of the 1st waypoint
+        transform.position = waypoints[waypointIndex].transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        EnemyMove(); 
     }
+
+    private void EnemyMove()
+    {
+        //     0,1,2            2
+        if(waypointIndex <= waypoints.Count-1)
+        {
+            //save the next waypoint as my target position
+            var targetPosition = waypoints[waypointIndex].transform.position;
+
+            targetPosition.z = 0f;
+            //set the enemy movement per frame
+            var enemyMovement = enemyMoveSpeed * Time.deltaTime;
+
+            //move from the current position, to the target position, at the enemyMovement speed
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, enemyMovement);
+
+            //if target waypoint is reached, update to the next waypoint
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+
+        }
+        //if enemy reaches the last waypoint
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
 }
