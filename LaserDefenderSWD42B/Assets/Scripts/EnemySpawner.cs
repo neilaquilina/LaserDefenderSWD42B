5a85478466bf7519c293eb231516e2fd14ae5286
neilaquilina <6985772+neilaquilina@users.noreply.github.com>
2020-11-18 14:24:59 +0100

    Spawn Enemies in Wave
    1. Using WaveConfig to get info about wave
    2. Spawn enemies in wave at waypoint(0)
    3. Using coroutines to spawn enemies depengding on time

---
+++ LaserDefenderSWD42B/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    //a list of Waves
+    [SerializeField] List<WaveConfig> waveConfigsList;
+
+    //always start from position 0
+    int startingWave = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set the currentWave to 0 (1st Wave)
+        var currentWave = waveConfigsList[startingWave];
+
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //call a coroutine to spawn Enemies in waveToSpawn
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveToSpawn)
+    {
+        //spawn enemies depending on the numberOfEnemies in waveToSpawn
+        for (int enemyCount = 1; enemyCount <= waveToSpawn.GetNumberOfEnemies(); enemyCount++)
+        {
+            //spawn enemy from waveToSpawn 
+            //at the position specified by the waypoint(0) of waveToSpawn
+            Instantiate(
+                waveToSpawn.GetEnemyPrefab(),
+                waveToSpawn.GetWaypoints()[0].transform.position,
+                Quaternion.identity);
+
+            yield return new WaitForSeconds(waveToSpawn.GetTimeBetweenSpawns());
+
+        }
+        
+    }
+        
+}
